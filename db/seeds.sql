15bf237813a9c227246af07b002a63c6c77f6996
David Thayer <dthayer16@gmail.com>
2019-08-25 16:37:28 -0700

    burger site functionality.

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ("Texas Chainsaw Massa-Curd Burger");
+INSERT INTO burgers (name) VALUES ("The Final Kraut Down Burger");
+INSERT INTO burgers (name) VALUES ("She’s a Super Leek Burger");
+INSERT INTO burgers (name) VALUES ("I Fought the Slaw Burger (and the Slaw Won)");
+INSERT INTO burgers (name) VALUES ("Kales From the Crypt Burger");
+INSERT INTO burgers (name) VALUES ("Foot Feta-ish Burger");
